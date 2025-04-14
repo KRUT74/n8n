012fd9a0a32818b59a1829a97aa3ebc10a496161
KRUT74 <sanka26@hotmail.com>
2025-04-14 14:44:02 +0200

    Add n8n.config.js and update start script to use it

---
+++ n8n.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  webhookUrl: 'https://n8n-krut74.onrender.com',
+  webhookTestUrl: 'https://n8n-krut74.onrender.com',
+  webhookTunnelUrl: 'https://n8n-krut74.onrender.com',
+  editorBaseUrl: 'https://n8n-krut74.onrender.com',
+  host: '0.0.0.0',
+  protocol: 'https',
+  port: 3000,
+  path: '/',
+  publicApiDisabled: false,
+  webhookDebug: true,
+  encryptionKey: process.env.N8N_ENCRYPTION_KEY,
+  database: {
+    type: 'postgresdb',
+    tablePrefix: '',
+    postgresdb: {
+      host: process.env.DB_POSTGRESDB_HOST,
+      port: process.env.DB_POSTGRESDB_PORT,
+      database: process.env.DB_POSTGRESDB_DATABASE,
+      user: process.env.DB_POSTGRESDB_USER,
+      password: process.env.DB_POSTGRESDB_PASSWORD,
+    },
+  },
+  nodes: {
+    exclude: [],
+    include: [],
+  },
+  security: {
+    basicAuth: {
+      active: true,
+      user: process.env.N8N_BASIC_AUTH_USER,
+      password: process.env.N8N_BASIC_AUTH_PASSWORD,
+    },
+  },
+  metrics: {
+    enabled: true,
+  },
+  diagnostics: {
+    enabled: true,
+  },
+  hiringBanner: {
+    enabled: false,
+  },
+  versionNotifications: {
+    enabled: true,
+  },
+  userManagement: {
+    disabled: false,
+  },
+}; 
